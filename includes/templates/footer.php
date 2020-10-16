fadd9532feaa491d260e7fc0d77a7bc0d612e02d
Alejandro <alejandro.montecillo@gmail.com>
2020-10-15 19:39:44 -0500

    Se integran templates
    Se crean archivos y templates para trabajar con php, se movio al servidor local el proyecto

---
+++ includes/templates/footer.php
@@ -0,0 +1,54 @@
+<footer class="site-header">
+        <div class="contenedor clearfix">
+            <div class="footer-informacion">
+                <h3>Sobre <span>GDLWebCamp</span></h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, illo! Voluptas, fugit rerum. Alias, vel nostrum, ut nemo blanditiis veniam explicabo eius ratione consequatur fugit laudantium id? Voluptatibus, quis. At!</p>
+            </div>
+            <div class="ultimos-tweet">
+                <h3>Ultimos<span>Tweets</span></h3>
+                <ul>
+                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quidem veniam dignissimos ipsum. Animi dignissimos iusto sint, esse culpa amet iste facere debitis quis, optio autem. Amet saepe fugiat dicta.</li>
+                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quidem veniam dignissimos ipsum. Animi dignissimos iusto sint, esse culpa amet iste facere debitis quis, optio autem. Amet saepe fugiat dicta.</li>
+                    <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quidem veniam dignissimos ipsum. Animi dignissimos iusto sint, esse culpa amet iste facere debitis quis, optio autem. Amet saepe fugiat dicta.</li>
+
+                </ul>
+            </div>
+            <div class="menu">
+                <h3>Redes <span>Sociales</span></h3>
+                <nav class="redes-sociales">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </nav>
+            </div>
+        </div>
+    </footer>
+    <p class="copyright">
+        Todos los Derechos Reservador GDLWebCamp 2020
+    </p>
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.lettering.js"></script>
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+        window.ga = function() {
+            ga.q.push(arguments)
+        };
+        ga.q = [];
+        ga.l = +new Date;
+        ga('create', 'UA-XXXXX-Y', 'auto');
+        ga('set', 'anonymizeIp', true);
+        ga('set', 'transport', 'beacon');
+        ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Muli:wght@200;400;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
+
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+
+    <header class="site-header">
+        <div class="hero">
+            <div class="contenido-header">
+                <nav class="redes-sociales">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </nav>
+                <div class="informacion-evento">
+                    <p class="fecha"><i class="far fa-calendar-alt"></i>18-12 Dic</p>
+                    <p class="ciudad"><i class="fas fa-map-marker-alt"></i> Guadalajara CDMX</p>
+                    <h1 class="nombre-sitio">GDLWebCamp</h1>
+                    <p class="slogan">La mejor conferencia de <span>Diseño Web</span></p>
+                </div>
+                <!--Informacion evento -->
+
+            </div>
+        </div>
+        <!--Hero-->
+    </header>
+
+    <div class="barra">
+        <div class="contenedor clearfix">
+            <div class="logo">
+                <a href="index.php">
+                <img src="img/logo.svg" alt="Logo GDLWebCamp">
+                </a>
+            </div>
+            <div class="menu-movil">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <nav class="navegacion-principal clearfix">
+                <a href="conferencia.php">Conferencia</a>
+                <a href="#">Calendario</a>
+                <a href="#">Invitado</a>
+                <a href="registro.php">Reservaciones</a>
+
+            </nav>
+        </div>
+    </div>
+    <!--Barra-->
