b34ba26f0a2e1631c2ea961cfdfd7ac6a03f6aab
Alejandro <alejandro.montecillo@gmail.com>
2020-10-17 04:04:49 -0500

    Fix: Port Database
    Se corrigio el puerto en la base y se crearon joins para mostrar los eventos del calendario

--- calendario.php
+++ calendario.php
@@ -7,7 +7,13 @@
           //Traemos el archivo que genera la conexion
           //a la base de datos
           require_once('includes/funciones/conexion.php');
-          $sql = "SELECT * from eventos"; 
+          $sql =" SELECT evento_id, nombre_evento,fecha_evento,hora_evento,cat_evento,nombre_invitado,apellido_invitado ";
+          $sql .=" FROM eventos ";
+          $sql .=" INNER JOIN categoria_evento ";
+          $sql .=" ON eventos.id_cat_evento= categoria_evento.id_categoria ";
+          $sql .=" INNER JOIN invitados";
+          $sql .=" ON eventos.invitado_id = invitados.invitado_id ";
+          $sql .="ORDER BY evento_id";
           //todo el código sql va en esta variable
           $resultado = $conn->query($sql);
           //En resultado se guarda el resultado del query 
@@ -22,15 +28,37 @@
         ?>
 
         <div class="calendario">
-          <?php  //fetch assoc es como arreglo 
-          //con el while es para mostrar todos 
-          //los registros
-          while ($eventos=$resultado->fetch_assoc()){ 
-          echo $eventos ['nombre_evento'];
-          echo "<br>";   
-            
-          }
-         ?>
+         <?php
+         $calendario = array(); 
+        while($eventos = $resultado->fetch_assoc()){ 
+          //obtener fecha del evento 
+          $fecha = $eventos['fecha_evento'];
+          
+          $evento = array(
+            'titulo' => $eventos['nombre_evento'],
+            'fecha' => $eventos['fecha_evento'],
+            'hora' => $eventos ['hora_evento'],
+            'categoria' => $eventos['cat_evento'],
+            'invitado' => $eventos['nombre_invitado'] ."". $eventos['apellido_invitado'],
+          );
+          //se agrupan los elementos de la misma fecha
+          $calendario [$fecha][]= $evento;
+          ?>
+        <?php }
+        //imprime todos los eventos 
+        foreach($calendario as $dia => $lista_eventos){ ?>
+          <h3>
+          <i class="far fa-calendar-alt"></i>
+          <?php echo $dia; ?>
+        </h3>
+        <?php} ?>
+        
+          <pre>
+            <?php 
+            var_dump($calendario)
+            ?>
+          </pre>
+
        </div>
         <?php
         //Siempre cerrar la base de datos
@@ -39,4 +67,3 @@
 
     </section>
     <?php include_once 'includes/templates/footer.php';?>
-  
--- includes/funciones/conexion.php
+++ includes/funciones/conexion.php
@@ -1,9 +1,9 @@
 <?php 
-  $conn=new mysqli('localhost','root','root','gdlweb');
-
+  $conn= mysqli_connect("localhost","root","root","gdlweb","3307");
+//agregar siempre el puerto
   if($conn->connect_error){
     //Se verifica que se pueda hacer la conexion a la 
     //base de datos
-    echo $error -> conn->connect_error;
+    echo $error-> $conn->connect_error;
   }
 ?>
